fe8b834e0ead61e78bfd726a3d8d1870eccda38e
muscle-control-gym <musclecontrolgym@gmail.com>
2024-11-07 16:08:17 +0800

    initial commit

---
+++ Trialscript.js
@@ -0,0 +1,90 @@
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30, // Adjust space between items as needed
+
+    // Pagination bullets
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Automatic swiper configuration
+    autoplay: {
+        delay: 3000, // Slide delay in milliseconds (3000ms = 3 seconds)
+        disableOnInteraction: false, // Continue autoplay even when interacting with the swiper
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1 // 1 item for small screens
+        },
+        
+        768: {
+            slidesPerView: 2 // 2 items for medium screens
+        },
+
+        1024: {
+            slidesPerView: 3 // 3 items for larger screens
+        }
+    }
+});
+
+
+//   all ------------------
+function initParadoxWay() {
+    "use strict";
+   
+    if ($(".testimonials-carousel").length > 0) {
+        var j2 = new Swiper(".testimonials-carousel .swiper-container", {
+            preloadImages: false,
+            slidesPerView: 1,
+            spaceBetween: 20,
+            loop: true,
+            grabCursor: true,
+            mousewheel: false,
+            centeredSlides: true,
+            pagination: {
+                el: '.tc-pagination',
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: '.listing-carousel-button-next',
+                prevEl: '.listing-carousel-button-prev',
+            },
+            breakpoints: {
+                1024: {
+                    slidesPerView: 3,
+                },
+                
+            }
+        });
+    }
+    
+// bubbles -----------------
+    
+    
+    setInterval(function () {
+        var size = randomValue(sArray);
+        $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
+        $('.individual-bubble').animate({
+            'bottom': '100%',
+            'opacity': '-=0.7'
+        }, 4000, function () {
+            $(this).remove()
+        });
+    }, 350);
+    
+}
+
+//   Init All ------------------
+$(document).ready(function () {
+    initParadoxWay();
+});
---
+++ script.js
@@ -0,0 +1,34 @@
+window.addEventListener('scroll', () => {
+    const sections = [
+        { id: 'homee' },
+        { id: 'AboutUs' },
+        { id: 'subscription' },
+        { id: 'ContactUs' },
+        { id:'feed'},
+        { id: 'APK' }
+    ];
+
+    const navLinks = document.querySelectorAll('.nav-links ul li a'); 
+
+    let currentSection = '';
+
+    sections.forEach((section, index) => {
+        const element = document.querySelector(`#${section.id}`);
+        const sectionTop = element.offsetTop;
+        const sectionHeight = element.clientHeight;
+
+        // Check if the current scroll position is within the section
+        const nextSectionTop = index + 1 < sections.length ? document.querySelector(`#${sections[index + 1].id}`).offsetTop : Infinity;
+
+        if (window.pageYOffset >= sectionTop - sectionHeight / .7 && window.pageYOffset < nextSectionTop - sectionHeight / 1) {
+            currentSection = section.id;
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.parentElement.classList.remove('active');
+        if (link.getAttribute('href') === `#${currentSection}`) {
+            link.parentElement.classList.add('active');
+        }
+    });
+});
